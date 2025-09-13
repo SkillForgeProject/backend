b78547dd7fe6b4f52bdca55beb69e2e367cfdedb
Juan Camilo Gallo <magita560@gmail.com>
2025-09-12 19:57:30 -0500

    Inizialicación proyecto

---
+++ src/test/java/com/eam/skillforge/SkillforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eam.skillforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

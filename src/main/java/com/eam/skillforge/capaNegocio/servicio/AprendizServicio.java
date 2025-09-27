40e800d0e6d4810c32aea509342f15636d12ee89
Juan Camilo Gallo <magita560@gmail.com>
2025-09-26 21:21:18 -0500

    ADD funcionalidad para obtener medallas por ID usuario

---
+++ src/main/java/com/eam/skillforge/capaNegocio/servicio/AprendizServicio.java
@@ -0,0 +1,9 @@
+package com.eam.skillforge.capaNegocio.servicio;
+
+import com.eam.skillforge.capaNegocio.dto.UsuarioDto;
+
+
+public interface AprendizServicio {
+
+    UsuarioDto getUsuarioPorId(Long id);
+}
---
+++ src/main/java/com/eam/skillforge/capaPersistencia/dao/AprendizDAO.java
@@ -0,0 +1,22 @@
+package com.eam.skillforge.capaPersistencia.dao;
+
+import com.eam.skillforge.capaNegocio.dto.UsuarioDto;
+import com.eam.skillforge.capaPersistencia.mapper.UsuarioMapper;
+import com.eam.skillforge.capaPersistencia.repositorio.AprendizRepositorio;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class AprendizDAO {
+    private final AprendizRepositorio aprendizRepositorio;
+    private final UsuarioMapper aprendizMapper;
+
+    public Optional<UsuarioDto> buscarPorId(Long id) {
+        return aprendizRepositorio.findById(id)
+                .map(aprendizMapper::toDto);
+    }
+
+}

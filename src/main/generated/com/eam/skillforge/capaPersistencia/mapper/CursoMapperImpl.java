6a8dc565fb249df3271a925753f2de2accbcccdc
Andrea Giraldo <andreagiraldo2391@gmail.com>
2025-09-27 22:53:43 -0500

    Cambios adicionales

--- src/main/generated/com/eam/skillforge/capaPersistencia/mapper/CursoMapperImpl.java
+++ src/main/generated/com/eam/skillforge/capaPersistencia/mapper/CursoMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-09-27T22:50:47-0500",
+    date = "2025-09-27T22:53:05-0500",
     comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.7 (Oracle Corporation)"
 )
 @Component
